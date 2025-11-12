3104aa5a64f0f87a4342c6ac136f1c8096a3f3c4
wumpfroot <itswumpa@gmail.com>
2025-11-12 04:27:33 +0200

    Flatpak added

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,9 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Enable Flatpaks
+  services.flatpak.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
